6ff09846986af99c0d431b92f9588c969ed69847
Rosen Georgiev <rosengeorgiew1@gmail.com>
2021-12-13 12:44:21 +0200

    Creation of Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDcm77FYKd_9CHdzPHhEAfb3pSpV2k_huo",
+    authDomain: "e-clone-5b08a.firebaseapp.com",
+    projectId: "e-clone-5b08a",
+    storageBucket: "e-clone-5b08a.appspot.com",
+    messagingSenderId: "87680833434",
+    appId: "1:87680833434:web:2bdb87f642d5ab901f0c23",
+    measurementId: "${config.measurementId}"
+};
