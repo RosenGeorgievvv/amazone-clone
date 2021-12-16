91551799bc22b2d681b0bf36d16a0c390c109442
Rosen Georgiev <rosengeorgiew1@gmail.com>
2021-12-16 10:51:22 +0200

    Home component created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
 import Header from './Header';
-
+import Home from './Home';
 
 
 function App() {
   return (
     <div className="app">
-      <h1> </h1>
-
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css';
+
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_container'>
+                <img className='home_image' src='https://m.media-amazon.com/images/I/71G7vKHnFZL._SX3000_.jpg' alt='home_image' />
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
