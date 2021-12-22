463a1122de01aca54c5f5640808b56d9208f0a8f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2021-12-22 12:12:40 +0200

    Product styling

--- src/Product.js
+++ src/Product.js
@@ -11,7 +11,13 @@ function Product() {
                     <small>$</small>
                     <strong>19.99</strong>
                 </p>
+
+                <div className='product_rating'>
+                    <p>⭐</p>
+                </div>
             </div>
+            <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' alt='' />
+            <button>Add to Basket</button>
         </div>
     )
 }
