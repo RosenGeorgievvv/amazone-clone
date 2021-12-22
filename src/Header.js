c30807e90ba713a79ab2adf674ef44e004126fcb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2021-12-22 17:39:16 +0200

    Checkout customization

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,15 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
+
 
 function Header() {
     return (
         <div className="header">
-            <img className="header_logo" src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+            <Link to="/">
+                <img className="header_logo" src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+            </Link>
 
             <div className="header_search">
                 <input className="header_searchInput" type="text" />
@@ -41,11 +45,13 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header_optionBasket">
-                <ShoppingBasketIcon />
-                <span className="header_optionLineTwo
+            <Link to="/checkout">
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo
                 header_basketCount">0</span>
-            </div>
+                </div>
+            </Link>
         </div>
     )
 }
