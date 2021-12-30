93a0d4baecc789ca7b7de3c2f22aeece0dc90b4f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2021-12-30 17:24:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Checkout from './Checkout';
+
 
 function App() {
   return (
