b6d5439cc6b2f80ab4ae0b178b8b921034936533
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-01-07 18:03:17 +0200

    login & register function implementation

--- src/Login.js
+++ src/Login.js
@@ -8,6 +8,14 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const signIn = e => {
+        e.preventDefault()
+    }
+
+    const register = e => {
+        e.preventDefault()
+    }
+
     return (
         <div className="login">
             <Link to={'/'}>
@@ -23,14 +31,14 @@ function Login() {
                     <h5>Password</h5>
                     <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className='login_signInButton'>Sign In</button>
+                    <button type='submit' className='login_signInButton' onClick={signIn}>Sign In</button>
 
                     <p>
                         By signing-in you agree to the AMAZON Fake Clone Conditions of Use & Sale.
                         Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                     </p>
 
-                    <button className='login_registerButton'>Create your Amazon Account</button>
+                    <button className='login_registerButton' onClick={register}>Create your Amazon Account</button>
                 </form>
             </div>
         </div>
