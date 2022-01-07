67c720ea620e60e7534b42debbda4046e77872d4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-01-07 11:54:20 +0200

    Add & Remove from basket func finished
    The whole process of adding an item to basket. The icon basket itself is being rendered once the add to basket button is clicked on the selected item. Then, clicking on the Basket Icon it directs you to basket page, where you could see the subtotal items and the calculated price. Moreover, if you want to delete an item from the basket, once you click on Delete from basket, the sum will be re-calculated and updated, either the subtotal items.

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='checkout'>
             <div className='checkout_left'>
@@ -14,6 +18,15 @@ function Checkout() {
                     <h2 className='checkout_title'>
                         Your Shopping Basket
                     </h2>
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
             <div className='checkout_right'>
