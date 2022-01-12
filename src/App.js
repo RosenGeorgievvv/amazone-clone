e97b4837226e2cf999dc1138069fde0f059fe56e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-01-12 17:17:15 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Checkout from './Checkout';
 import Login from './Login'
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 function App() {
+
+  useEffect(() => {
+
+    //will only run once when the app component loads
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+      } else {
+        // the user is logged out
+      }
+    })
+  }, [])
   return (
     <Router>
       <div className="app">
