594760479886771d7c4ce49f11fb0f3383b77f6f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-01-20 11:58:28 +0200

    slight changes

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
 import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct'
-import FlipMove from 'react-flip-move'
+
 
 function Checkout() {
     const [{ basket, user }, dispatch] = useStateValue();
