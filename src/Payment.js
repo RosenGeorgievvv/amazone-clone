7f5629e57d103f409bed87b023e483980335a90f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-02-01 19:47:51 +0200

    Payment page finished

--- src/Payment.js
+++ src/Payment.js
@@ -4,6 +4,8 @@ import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
 
 function Payment() {
 
@@ -12,6 +14,8 @@ function Payment() {
     const stripe = useStripe();
     const elements = useElements();
 
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(true);
 
@@ -43,17 +47,17 @@ function Payment() {
                 <div className='payment_section'>
                     <div className='payment_title'>
                         <h3>Review items and delivery</h3>
-                        <div className='payment_items'>
-                            {basket.map(item => (
-                                <CheckoutProduct
-                                    id={item.id}
-                                    title={item.title}
-                                    image={item.image}
-                                    price={item.price}
-                                    rating={item.rating}
-                                />
-                            ))}
-                        </div>
+                    </div>
+                    <div className='payment_items'>
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div className='payment_section'>
@@ -65,8 +69,21 @@ function Payment() {
                             <CardElement onChange={handleChange} />
 
                             <div className='payment_priceContainer'>
-
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                </button>
                             </div>
+                            {error && <div>{error}</div>}
                         </form>
 
                     </div>
